796be2de6576ae78f40445f3126c7f5bcd65e4c9
arios <andres.rios@ctg.com>
2023-05-04 23:22:58 -0500

    initial commit

---
+++ src/main/java/org/javaFaker/test/Employee.java
@@ -0,0 +1,15 @@
+package org.javaFaker.test;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+    String firstName;
+    String lastName;
+    String interest;
+    String phoneNumber;
+    String address;
+    String email;
+    String designation;
+}
---
+++ src/main/java/org/javaFaker/test/EmployeeRegistrationTest.java
@@ -0,0 +1,33 @@
+package org.javaFaker.test;
+
+import com.github.javafaker.Faker;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class EmployeeRegistrationTest {
+
+    Faker fakerData = new Faker();
+    Employee employee;
+
+    @BeforeMethod
+    public void generateData() {
+        employee = new Employee();
+        employee.setFirstName(fakerData.name().firstName());
+        employee.setLastName(fakerData.name().lastName());
+        employee.setInterest(fakerData.internet().domainWord());
+        employee.setPhoneNumber(fakerData.phoneNumber().phoneNumber());
+        employee.setAddress(fakerData.address().fullAddress());
+        employee.setEmail(fakerData.internet().emailAddress());
+        employee.setDesignation(fakerData.job().position());
+    }
+
+    @Test
+    public void registerEmployee() {
+        System.out.println(employee.getFirstName());
+    }
+
+    @Test
+    public void registerEmployee2() {
+        System.out.println(employee.getFirstName());
+    }
+}
